27fc97eee3f4b2fb16b1f1537e3efea7a9029025
Tímea Tóth <tiemka14@gmail.com>
2025-12-01 22:04:32 +0100

    chore: activate conda env in start.sh before starting UVicorn

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+# Activate conda environment created in Docker image and start the server
+if [ -f /opt/conda/etc/profile.d/conda.sh ]; then
+	. /opt/conda/etc/profile.d/conda.sh
+	conda activate IMAGDressing || true
+fi
+
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
